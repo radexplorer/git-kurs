d3e8eab9089f1ad30d28187fc687d7a7bb181a53
Radosław Rudnik <rudnikrado@gmail.com>
2024-02-24 16:11:31 +0100

    Jenkins completed

---
+++ folder1/jenkinsfile.groovy
@@ -0,0 +1,26 @@
+pipeline {
+    agent any
+    stages {
+        stage('Build') {
+            steps {
+                script {
+                    echo 'Building...'
+                }
+            }
+        }
+        stage('Test') {
+            steps {
+                script {
+                    echo 'Testing...'
+                }
+            }
+        }
+        stage('Deploy') {
+            steps {
+                script {
+                    echo 'Deploying...'
+                }
+            }
+        }
+    }
+}
