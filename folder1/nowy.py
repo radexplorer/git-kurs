55320238b9d78295f510c64f00e3d263f80ec24e
Radosław Rudnik <rudnikrado@gmail.com>
2024-04-16 14:35:45 +0200

    Nowy.oy file created

---
+++ folder1/nowy.py
@@ -0,0 +1,23 @@
+
+print("lalal")
+
+czyDuży = True
+liczba = 2**3
+a = liczba
+
+
+print(a)
+
+cenaNettpJava = 100
+podatek = 1.23
+cenaBruttoJava = cenaNettpJava*podatek
+print(cenaBruttoJava)
+print(czyDuży)
+
+imie = "Paula"
+print(imie[-1])
+print(imie[1:3])
+
+###################
+import math 
+help(math.ceil)
