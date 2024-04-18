09a3acbd81a9bf18131c9242867685e39a8125c0
Radosław Rudnik <rudnikrado@gmail.com>
2024-04-18 21:05:38 +0200

    Online course update v.2

--- folder1/nowy.py
+++ folder1/nowy.py
@@ -37,6 +37,7 @@ while liczba >= 0:
     print(liczba)
     liczba -= 1
 '''
+'''
 wynik = 0
 
 i=0
@@ -46,3 +47,68 @@ while i<4:
     wynik += x
     i += 1
 print("Wynink to : ", wynik)
+'''
+'''
+wynik = 0 
+
+for i in range (200):
+    if (i % 5 == 0 and i % 7 != 0):
+        print(("liczba", i, "podz. przez 5 i niepodz. przez 7"))
+print("Wynik to: ", wynik)
+'''
+
+#### BREAK AND CONTINUE
+'''
+wynik = 0 
+
+for i in range (3):
+    x=int(input("Give number bigger than 0: "))
+    if (x>0):
+        wynik += x
+    else:
+        print("Miała byc liczba dodatnia")
+        break
+    print("Aktualny wynik dodawania to: ", wynik)
+'''
+'''
+wynik = 0
+i = 0
+
+while i < 3:
+    x=int(input("Give number bigger than 0: "))
+    if (x>0):
+        wynik += x
+    else:
+        print("Miała byc liczba dodatnia")
+        continue
+    print("Aktualny wynik dodawania to: ", wynik)
+    i += 1
+
+'''
+'''
+wynik = 0
+i = 0
+
+while i < 3:
+    x=int(input("podaj parzystą liczbę: "))
+    if (x>0 and x%2==0):
+        wynik += x
+    else:
+        print("Miała być parzysta")
+        continue
+    print("Aktualny wynik dodawania to: ", wynik)
+    i += 1
+'''
+#Zgadywana liczba
+
+szukanaLiczba = 40
+zgdaywanaLiczba = 0
+
+while zgdaywanaLiczba != szukanaLiczba:
+    zgdaywanaLiczba = int(input("Odgadnij liczbę: "))
+
+    if (zgdaywanaLiczba > szukanaLiczba):
+        print("Mniej")
+    elif (zgdaywanaLiczba <)
+    else:
+        print("Try again")
